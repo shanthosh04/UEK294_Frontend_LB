af1b4dbc73d678193416f9b275c5884fd5493e59
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-11-14 19:45:15 +0100

    implemtens Guards 6.1

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
 
+
+function hasClaim(admin: string) {
+  return false;
+}
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+
+  if(localStorage.getItem("ACCESS_TOKEN") && hasClaim("admin")){
+    return true;
+  }
+  router.navigateByUrl('/login');
+  return false;
 };
+

c4525b8fc47a8e4265233d74e690b5053c5f1641
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-11-15 09:54:13 +0100

    implemtens tailwind 7.1

--- src/app/pages/category/category-modify/category-modify.component.ts
+++ src/app/pages/category/category-modify/category-modify.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { CategoryControllerService } from "../../../openapi-client";
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'pm-category-modify',
   templateUrl: './category-modify.component.html',
   styleUrls: ['./category-modify.component.scss']
 })
-export class CategoryModifyComponent {
 
+export class CategoryModifyComponent {
+  isEdit: boolean = false;
+  formGroup: FormGroup = new FormGroup({});
+  categoryId: string | number | undefined;
+
+  constructor(
+    private readonly categoryControllerService: CategoryControllerService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    if (this.activatedRoute.snapshot.params['id']) {
+      this.isEdit = true;
+
+      this.categoryControllerService.getCategoryById(this.activatedRoute.snapshot.params['id']).subscribe(category => {
+        this.formGroup.patchValue(category);
+        this.categoryId = category.id;
+      });
+    }
+  }
 }
+

e481e601ca439c924da3375ec82c4de69c161d69
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-11-18 19:46:39 +0100

    implemtens guard HasClaim

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,32 @@
-import {CanActivateFn, Router} from '@angular/router';
-import {inject} from "@angular/core";
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { jwtDecode } from "jwt-decode";
 
+interface JwtPayload {
+  exp: number;
+  claims: string[];
+}
 
-function hasClaim(admin: string) {
-  return false;
+function hasClaim(claimType: string): boolean {
+  const token = localStorage.getItem('ACCESS_TOKEN');
+  if (!token) return false;
+
+  try {
+    const decoded: JwtPayload = jwtDecode(token);
+    return decoded.claims && decoded.claims.includes(claimType);
+  } catch (error) {
+    console.error('Could not decode token', error);
+    return false;
+  }
 }
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
-  if(localStorage.getItem("ACCESS_TOKEN") && hasClaim("admin")){
+  if (localStorage.getItem('ACCESS_TOKEN') && hasClaim('admin')) {
     return true;
   }
+
   router.navigateByUrl('/login');
   return false;
 };
-
